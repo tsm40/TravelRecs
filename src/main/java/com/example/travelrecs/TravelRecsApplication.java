f52e83c5f20c762414c13195bd9a27885c0e332c
yushi111 <andrewsys123@gmail.com>
2024-10-31 23:20:04 -0400

    Init

---
+++ src/main/java/com/example/travelrecs/TravelRecsApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelrecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelRecsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelRecsApplication.class, args);
+    }
+
+}
